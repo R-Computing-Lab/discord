e76081e909cc300dcff3099788457482a65f48d7
JT <jonathan@zenspirations.com>
2020-07-06 14:54:20 -0400

    Started new scripts for updated CRAN package
    @smasongarrison, please add descriptions to each in some amount of detail so we can start from scratch!

---
+++ Updated R/discord_data.R
@@ -0,0 +1,4 @@
+### Title: discord_data.R
+### Author: S. Mason Garrison & Jonathan Trattner
+### Date: July 6th, 2020
+### Description: Function to ___________.
---
+++ Updated R/discord_regression.R
@@ -0,0 +1,5 @@
+### Title: discord_regression.R
+### Author: S. Mason Garrison & Jonathan Trattner
+### Date: July 6th, 2020
+### Description: Function to ___________.
+
