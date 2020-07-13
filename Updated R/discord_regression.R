e4f3ad918a1dfabf631abe7d83d74d92225db820
Mason Garrison <s.mason.garrison@gmail.com>
2020-07-13 10:18:17 -0400

    Update discord_regression.R

--- Updated R/discord_regression.R
+++ Updated R/discord_regression.R
@@ -1,5 +1,5 @@
 ### Title: discord_regression.R
 ### Author: S. Mason Garrison & Jonathan Trattner
 ### Date: July 6th, 2020
-### Description: Function to ___________.
+### Description: Function to apply discordant regression to discordant data.
 
