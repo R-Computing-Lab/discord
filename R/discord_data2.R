17d783faef1c107722ab44e0db2d95d489e322d8
Mason Garrison <s.mason.garrison@gmail.com>
2020-07-15 11:32:54 -0400

    tinkering

--- R/discord_data2.R
+++ R/discord_data2.R
@@ -15,126 +15,59 @@
 #'\item{predictor_i_diff}{difference between predictor i of kin1 and kin2}
 #'\item{predictor_i_mean}{mean predictor i for kin1 and kin2}
 
-
+df <- read.csv("E:/Dropbox/Lab/zSoftware/Github/discord/hidden/dsDouble_G1.csv", stringsAsFactors=TRUE)
 
 discord_data2<- function(
   outcome,
   predictors=NULL,
-  doubleentered=F,
+  doubleentered=FALSE,
   sep="",
-  scale=T,
+  scale=TRUE,
   df=NULL,
   id=NULL,
-  full=T,
+  full=TRUE,
+  NLSY=TRUE,
   ...){
-  arguments <- as.list(match.call())
-  y <- ysort <- NULL
 
-  IVlist <- list()
-  outcome1=subset(df, select=paste0(arguments$outcome,sep,"1"))[,1]
-  outcome2=subset(df, select=paste0(arguments$outcome,sep,"2"))[,1]
+  #creat variable names
+  outcome_diff=paste0(outcome,"_diff")
+  outcome_mean=paste0(outcome,"_mean")
+  predictors_mean=paste0(predictors,"_mean")
+  predictors_diff=paste0(predictors,"_diff")
 
-  #create id if not supplied
-  if(is.null(id))
-  {
-    id<-rep(1:length(outcome1[,1]))}
-  #If no predictors selected, grab all variables not listed as outcome, and contain sep 1 or sep 2
-  if(is.null(predictors)){
-    predictors<-setdiff(unique(gsub(paste0(sep,"1|",sep,"2"),"",grep(paste0(sep,"1|",sep,"2"),names(df),value = TRUE))),paste0(arguments$outcome))
-    #unpaired.predictors=setdiff(grep(paste0(sep,"1|",sep,"2"),names(df),value = TRUE,invert=TRUE),paste0(arguments$id))
-  }
+  ## creates all combos, instead of var1_s1 var2_s2
+  outcome_s1s2=do.call(paste0, expand.grid(outcome, "_S",1:2))
+  predictors_s1s2=do.call(paste0, expand.grid(predictors, "_S",1:2))
 
 
-  if(!doubleentered){
-    outcome2x<-outcome2
-    outcome2<-c(outcome2[,1],outcome1[,1])
-    outcome1<-c(outcome1[,1],outcome2x[,1])
+  #If data iss NLSY
+if(NLSY){
 
-    if(scale&is.numeric(outcome1)){
-      outcome1<-scale(outcome1)
-    outcome2<-scale(outcome2)
+vars_keep=c("SubjectTag_S1",
+            "SubjectTag_S2",
+            "ExtendedID",
+            "R",
+            "RelationshipPath",
+            "RACE_S1",
+            "SEX_S1",
+            "RACE_S2",
+            "SEX_S2")
+
+
+mutate(df, outcome_diff = .data[[outcome[[1]]]] - .data[[vars[[2]]]])
+mutate(df,mass_norm = mass / mean(mass, na.rm = TRUE))
+
+  # keep essential nlsy variables
+df_final = select(df,
+                  vars_keep)
+
+
+
+
+}else{ #If data is not NLSY
     }
-    DV<-data.frame(outcome1,outcome2)
-    DV$outcome_diff<- DV$outcome1-DV$outcome2
-    DV$outcome_mean<-(DV$outcome1+DV$outcome2)/2
-
-    remove(outcome1);remove(outcome2x);remove(outcome2)
-
-    for(i in 1:length(predictors)){
-
-      predictor1x= predictor1=subset(df, select=paste0(predictors[i],sep,"1"))[,1]
-      predictor2=subset(df, select=paste0(predictors[i],sep,"2"))[,1]
-      predictor1<-c(predictor1[,1],predictor2[,1])
-      predictor2<-c(predictor2[,1],predictor1x[,1])
-      if(scale&is.numeric(predictor1)){
-        predictor1<-scale(predictor1)
-        predictor2<-scale(predictor2)
-      }
-      remove(predictor1x)
-      IVi<-data.frame(predictor1,predictor2)
-      IVi$predictor_diff<-IVi$predictor1-IVi$predictor2
-      IVi$predictor_mean<-(IVi$predictor1+IVi$predictor2)/2
-      names(IVi)<-c(paste0(predictors[i],"_1"),paste0(predictors[i],"_2"),paste0(predictors[i],"_diff"),paste0(predictors[i],"_mean"))
-      IVlist[[i]] <- IVi
-
-      names(IVlist)[i]<-paste0("")
-    }
-  }else{
-
-    if(scale&is.numeric(outcome1))
-
-    {outcome1<-scale(outcome1)
-    outcome2<-scale(outcome2)
-    }
-    DV<-data.frame(outcome1,outcome2)
-
-    DV$outcome_diff<-DV$outcome1-DV$outcome2
-    DV$outcome_mean<-(DV$outcome1+DV$outcome2)/2
-
-    remove(outcome1);remove(outcome2)
-    for(i in 1:length(predictors)){
-      predictor1=subset(df, select=paste0(predictors[i],sep,"1"))[,1]
-      predictor2=subset(df, select=paste0(predictors[i],sep,"2"))[,1]
-      if(scale&is.numeric(predictor1))
-      {predictor1<-scale(predictor1)
-      predictor2<-scale(predictor2)
-      }
-      IVi<-data.frame(predictor1,predictor2)
-      IVi$predictor_diff<-IVi$predictor1-IVi$predictor2
-      IVi$predictor_mean<-(IVi$predictor1+IVi$predictor2)/2
-      names(IVi)<-c(paste0(predictors[i],"_1"),paste0(predictors[i],"_2"),paste0(predictors[i],"_diff"),paste0(predictors[i],"_mean"))
-      IVlist[[i]] <- IVi
-      names(IVlist)[i]<-paste0("")
-    }
-  }
 
 
-  DV$id<-id
-  DV$ysort<-0
-  DV$ysort[DV$outcome_diff>0&!is.na(DV$outcome_diff)]<-1
-
-  # randomly select for sorting on identical outcomes
-
-  if(length(unique(DV$id[DV$outcome_diff==0]))>0){
-    select<-sample(c(0,1), replace=TRUE, size=length(unique(DV$id[DV$outcome_diff==0&!is.na(DV$outcome_diff)])))
-    DV$ysort[DV$outcome_diff==0&!is.na(DV$outcome_diff)]<-c(select,abs(select-1))
-
-  }
-  DV$id<-NULL
-  names(DV)<-c(paste0(arguments$outcome,"_1"),paste0(arguments$outcome,"_2"),paste0(arguments$outcome,"_diff"),paste0(arguments$outcome,"_mean"),"ysort")
-
-  merged.data.frame =data.frame(id,DV,IVlist)
-
-  id<-ysort<-NULL #appeases R CMD check
-
-  merged.data.frame<-subset(merged.data.frame,ysort==1)
-  merged.data.frame$ysort<-NULL
-  merged.data.frame <- merged.data.frame[order(merged.data.frame$id),]
-  if(!full)
-  {varskeep<-c("id",paste0(arguments$outcome,"_diff"),paste0(arguments$outcome,"_mean"),paste0(predictors,"_diff"),paste0(predictors,"_mean"))
-
-  merged.data.frame<-merged.data.frame[varskeep]
-  }
 
   return(merged.data.frame)
 }
