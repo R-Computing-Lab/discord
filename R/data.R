3758205c906b3a72d5162e21886f8494fc31674f
Jonathan Trattner <accounts@jdtrat.com>
2020-08-12 12:34:25 -0400

    Added sample data for us to check the package development on. I also got rid of many of the warnings/notes when running devtools::check(). We need to document the arguments for some of the functions still, though.

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Sample Data from NLSY
+#'
+#' A dataframe output from the original
+#' \code{CreatePairLinksDoubleEntered} function that the original
+#' \code{\link{discord_data}} function will work on. It contains percentile
+#' information for education, occupation, total net family income, and a raw SES
+#' score.
+#'
+#' @format A data frame with 50 rows and 63 columns \describe{ Sibling Pairs and
+#'   their percentile for education, occupation, total net family income, and
+#'   SES for the years 2002, 2004, 2006, and 2008. It also contains individuals'
+#'   race and sex. For race, the NLSY categorizes are: }
+#' @source NLSY/R Lab
+"sampleData"
