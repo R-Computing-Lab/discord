00961e409fc8ce4555ea59984b4cf7adce09350a
Jonathan Trattner <accounts@jdtrat.com>
2020-10-09 13:54:48 -0400

    Added new data for testing (uniqueExtendedIDs and moreUniqueExtendedIDs)

--- R/data.R
+++ R/data.R
@@ -12,3 +12,21 @@
 #'   race and sex. For race, the NLSY categorizes are: }
 #' @source NLSY/R Lab
 "sampleData"
+
+#' Unique IDs Derived from sampleData
+#'
+#' A dataframe output from the original
+#' \code{CreatePairLinksDoubleEntered} function that the original
+#' \code{\link{discord_data}} function will work on. It contains percentile
+#' information for education, occupation, total net family income, and a raw SES
+#' score.
+#'
+#' @format A dataframe for testing with 32 rows
+#' @source NLSY/R Lab
+"uniqueExtendedIDs"
+
+#' More Unique IDs Derived from sampleData
+#'
+#' @format A dataframe for testing with 1200 rows (just multiples of uniqueExtendedIDs)
+#' @source NLSY/R Lab
+"moreUniqueExtendedIDs"
