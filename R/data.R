f61e37efb71c578cec62837bff627aca57d14c8b
Jonathan Trattner <accounts@jdtrat.com>
2020-11-02 16:43:44 -0500

    Updated data documentation

--- R/data.R
+++ R/data.R
@@ -1,32 +1,9 @@
 #' Sample Data from NLSY
 #'
-#' A data frame output from the original
-#' \code{CreatePairLinksDoubleEntered} function that the original
-#' \code{\link{discord_data}} function will work on. It contains percentile
-#' information for education, occupation, total net family income, and a raw SES
-#' score.
+#' A data frame output from \link[NlsyLinks]{CreatePairLinksSingleEntered} that
+#' contains data for kinship pairs' height and weight.
 #'
-#' @format A data frame with 50 rows and 63 columns \describe{Sibling Pairs and
-#'   their percentile for education, occupation, total net family income, and
-#'   SES for the years 2002, 2004, 2006, and 2008. It also contains individuals'
-#'   race and sex. For race, the NLSY categorizes are: }
+#' @format A data frame. \describe{Kinship pairs and their relatedness, height,
+#'   and weight information.}
 #' @source NLSY/R Lab
-"sampleData"
-
-#' Unique IDs Derived from sampleData
-#'
-#' A dataframe output from the original
-#' \code{CreatePairLinksDoubleEntered} function that the original
-#' \code{\link{discord_data}} function will work on. It contains percentile
-#' information for education, occupation, total net family income, and a raw SES
-#' score.
-#'
-#' @format A dataframe for testing with 32 rows
-#' @source NLSY/R Lab
-"uniqueExtendedIDs"
-
-#' More Unique IDs Derived from sampleData
-#'
-#' @format A dataframe for testing with 1200 rows (just multiples of uniqueExtendedIDs)
-#' @source NLSY/R Lab
-"moreUniqueExtendedIDs"
+"sample_data"
