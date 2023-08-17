9995380b5880b4a76752cbfdd6d1a76577d4b2a4
Mason Garrison <garrissm@wfu.edu>
2023-08-17 10:34:08 -0400

    added toggle

--- R/func_discord_regression.R
+++ R/func_discord_regression.R
@@ -1,7 +1,7 @@
 #' Perform a Linear Regression within the Discordant Kinship Framework
 #'
 #' @inheritParams discord_data
-
+#' @param data_processed Logical operator if data are already preprocessed by discord_data , default is FALSE
 #' @return Resulting `lm` object from performing the discordant regression.
 #'
 #' @export
@@ -21,7 +21,8 @@ discord_regression <- function(data,
                                id = NULL,
                                sex = "sex",
                                race = "race",
-                               pair_identifiers = c("_s1", "_s2")) {
+                               pair_identifiers = c("_s1", "_s2"),
+                               data_processed = FALSE) {
 
   check_discord_errors(data = data, id = id, sex = sex, race = race, pair_identifiers = pair_identifiers)
 
@@ -34,7 +35,7 @@ discord_regression <- function(data,
   } else if (!is.null(sex) & !is.null(race)) {
     demographics <- "both"
   }
-
+if (!data_processed) {
   preppedData <- discord_data(data = data,
                               outcome = outcome,
                               predictors = predictors,
@@ -43,7 +44,9 @@ discord_regression <- function(data,
                               race = race,
                               pair_identifiers = pair_identifiers,
                               demographics = demographics)
-
+} else{
+  preppedData <- data
+  }
   # Run the discord regression
   realOutcome <- base::paste0(outcome, "_diff")
   predOutcome <- base::paste0(outcome, "_mean")
@@ -63,6 +66,7 @@ discord_regression <- function(data,
     demographic_controls <- base::paste0(sex, "_1 + ", race, "_1 + ", sex, "_2")
     preds <- base::paste0(predOutcome, " + ", pred_diff, " + ", pred_mean, " + ", demographic_controls)
   }
+
 
   model <- stats::lm(stats::as.formula(paste(realOutcome, preds, sep = " ~ ")), data = preppedData)
 
